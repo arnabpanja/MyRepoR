71ba696442b1852285c9b02c39336897c14b864c
arnabpanja <arnab.panja@gmail.com>
2022-05-06 18:53:48 +0530

    Health Claims data analysis - Added a dumbbell chart

--- scripts/andhra_health_data_analysis.R
+++ scripts/andhra_health_data_analysis.R
@@ -1,5 +1,10 @@
 suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(janitor))
+suppressPackageStartupMessages(library(ggbump))
+suppressPackageStartupMessages(library(ggridges))
+
+
+
 
 
 # Load source data -------------------------
@@ -97,7 +102,7 @@ p_surgeries_district <- andhra_health_data |> select(surgery_date, district_name
   summarise(count = n(), .groups = "drop") |> ungroup() |> 
   mutate(surgery_year = as.numeric(surgery_year)) |> 
   ggplot() + 
-  ggbump::geom_bump(mapping = aes(x = surgery_year, 
+  geom_bump(mapping = aes(x = surgery_year, 
                                   y = count, 
                                   color = district_name), size = 1) + 
   geom_point(mapping = aes(x = surgery_year, 
@@ -108,7 +113,7 @@ p_surgeries_district <- andhra_health_data |> select(surgery_date, district_name
   labs(color = "District", 
        x = "Year of Surgery", 
        y = "Number of Surgeries", 
-       title = "Surgeries by District", 
+       title = "Health Claims - Surgeries by District", 
        subtitle = "Big bump in Surgeries in some districts")
 
 
@@ -134,9 +139,9 @@ andhra_health_data |> select(surgery_date, district_name) |>
 
 p_surgery_hospital <- andhra_health_data |> group_by(category_name, hosp_type) |> 
   summarise(count = n(), .groups = "drop") |> ungroup() |> 
-  mutate(category_name = str_replace_all(string = category_name, 
+  mutate(category_name = str_trim(str_replace_all(string = category_name, 
                                          pattern = "SURGERY|AND|SURGERIES|DISEASES|SURGICAL|PROCEDURES", 
-                                         replacement = ""), 
+                                         replacement = "")), 
          hosp_type = case_when(
            hosp_type == "C" ~ "C - Private", 
            hosp_type == "G" ~ "G - Government", 
@@ -153,7 +158,7 @@ p_surgery_hospital <- andhra_health_data |> group_by(category_name, hosp_type) |
   theme_minimal() + 
   theme(legend.position = "top") + 
   labs(fill = "Hospital Type", 
-       title = "Surgery by Hospital Type", 
+       title = "Health Claims - Surgery by Hospital Type", 
        y = NULL, 
        x = "Number of Surgeries")
 
@@ -162,14 +167,66 @@ p_surgery_hospital
 ggsave(filename = "plots/andhra_health_insurance/p_surgery_hospital.png", 
        plot = p_surgery_hospital)
 
-  
+
+
+# Ridge Chart -----
+# to show age distribution by sex and mortality 
+
+p_age_mortality_sex <- andhra_health_data |> select(new_sex, mortality_y_n, age) |> 
+  ggplot() + 
+  ggridges::geom_density_ridges2(mapping = aes(x = age, 
+                                              y = mortality_y_n, 
+                                              fill = new_sex), 
+                                na.rm = TRUE, 
+                                alpha = 0.6, 
+                                scale = 0.75, 
+                                bandwidth = 4) +
+  scale_fill_brewer(palette = "Spectral") + 
+  scale_x_continuous(breaks = seq(0, 120, 20)) + 
+  theme_minimal() + 
+  theme(legend.position = "top") + 
+  labs(fill = "Sex", 
+       x = "Age", 
+       y = "Mortality", 
+       title = "Health Claims - Mortatlity Distribution with Age & Sex")
+
+ggsave(filename = "plots/andhra_health_insurance/p_age_mortality_sex.png", 
+       plot = p_age_mortality_sex)
 
 
 
+# Dumbbell Chart -----
+# to show minimum and maximum claim amounts per surgery category 
 
 
+p_claim_amt_surgery <- andhra_health_data |> select(category_name, claim_amount) |> 
+  mutate(category_name = str_trim(str_replace_all(string = category_name, 
+                                         pattern = "SURGERY|AND|SURGERIES|DISEASES|SURGICAL|PROCEDURES", 
+                                         replacement = ""))) |> 
+  group_by(category_name) |> 
+  summarise(min_claim = min(claim_amount), 
+            max_claim = max(claim_amount), .groups = "drop") |>  
+  ggplot() + 
+  ggalt::geom_dumbbell(mapping = aes(y = category_name, 
+                                     x = min_claim, 
+                                     xend = max_claim), 
+                       size = 1.5, 
+                       color = "blue", 
+                       size_x = 3, 
+                       size_xend = 4, 
+                       colour_x = "green", 
+                       colour_xend = "magenta") + 
+  scale_x_continuous(labels = scales::comma) + 
+  theme_minimal() + 
+  labs(x = "Claim Amounts (in Indian Rupees)", 
+       y = NULL,  
+       title = "Health Claims - Claims Amounts", 
+       subtitle = "Variation of Claim Amounts by Surgery Category")
 
+p_claim_amt_surgery
 
+ggsave(filename = "plots/andhra_health_insurance/p_claim_amt_surgery.png", 
+       plot = p_claim_amt_surgery)
 
 
 
