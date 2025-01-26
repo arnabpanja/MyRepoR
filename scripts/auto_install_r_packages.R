468ca6ab269bcd667093590366e0e88711c68aa6
arnabpanja <arnab.panja@gmail.com>
2025-01-26 12:56:12 +0530

    R Upgrade to version 4.4.2

--- scripts/auto_install_r_packages.R
+++ scripts/auto_install_r_packages.R
@@ -1,5 +1,6 @@
 # WRITE THE LIST OF PACKAGES INSTALLED  ----
-
+# DO THIS AFTER R IS UPGRADED TO THE NEW VERSION
+# OTHERWISE PICK UP THE EXECUTION FROM LINE 23 
 
 # get the list of installed packages ----------
 installed_pkgs_df <- as.data.frame(installed.packages(), 
@@ -30,8 +31,16 @@ pkg_df_list <- read.csv(file = file_name,
                         stringsAsFactors = FALSE)
 
 # get the packages that were installed by the user -----
-pkg_list_vec <- pkg_df_list[is.na(pkg_df_list$Priority), c("Package"), drop = TRUE]
+pkg_list_vec <- sort(pkg_df_list[is.na(pkg_df_list$Priority), 
+                                 c("Package"), 
+                                 drop = TRUE])
 
+
+
+# declare a list with defined list to store the installation statuses
+
+pkg_installed_status_list <- vector(mode = "list", 
+                                    length = length(pkg_list_vec))
 
 # try installing the list of packages ------------
 for (i in seq_along(pkg_list_vec)){
@@ -40,21 +49,27 @@ for (i in seq_along(pkg_list_vec)){
   if(i == 1) 
     pkg_install_status_df <- data.frame()
   
+  # send a message to the terminal -----
+  cat(paste0("INSTALLING PACKAGE ", pkg_list_vec[i], " .... "))
+  
   # install the packages ----
   install.packages(pkg_list_vec[i])
   
   # get the list of installed packages to find if it was installed ----
-  get_pkg_vec <- as.data.frame(installed.packages(), 
-                               stringsAsFactors = FALSE)[, "Package", drop = TRUE] 
+  get_pkg_vec <- installed.packages()[, "Package", drop = TRUE] 
   
   # populate the status of installation ----
-  pkg_install_status_df <- rbind(pkg_install_status_df, 
-                                 data.frame(pkg = pkg_list_vec[i], 
+  pkg_installed_status_list[[i]] <- c(pkg = pkg_list_vec[i], 
                                             status = ifelse(pkg_list_vec[i] %in% get_pkg_vec, 
                                                             "installed", 
-                                                            "not installed")))
+                                                            "not installed"))
+  
   
 }
 
+# create the data frame with the list elements ---- 
+pkg_install_status_df <- do.call(rbind, pkg_installed_status_list)
+
 # print the status of the installation ----
-pkg_install_status_df
+pkg_install_status_df |> print()
+
